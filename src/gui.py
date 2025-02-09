347ed9e5a2a7cea7ea61ccb35bdcce964514155a
GenyaElamkov <genyaElamkov@gmail.com>
2025-02-09 10:32:39 +0300

    refacroring

--- src/gui.py
+++ src/gui.py
@@ -9,8 +9,7 @@ class Window(ttk.Frame):
     def __init__(self, master=None):
         super().__init__()
         self.help_label = ttk.Label(
-            text="1. Нажмите кноку «Выбрать файл» — выберите файл — exel\n2. Нажмите кноку «Запустить»\n"
-        )
+            text="1. Нажмите на кнопку «Выбрать файл» и найдите ваш Excel файл.\n2. Прежде чем запускать обработку, убедитесь, что в файле нет пустых строк в верхней части.\n3. Когда все готово, нажмите кнопку «Запустить».")
         self.text_label = ttk.Label(text="Выберите файл")
 
         self.filepath = ""
@@ -33,16 +32,26 @@ class Window(ttk.Frame):
             self.text_label.config(text="Не выбран файл")
 
     def setup_grid(self):
-        self.help_label.grid(column=0, row=0, padx=10)
-        self.text_label.grid(column=0, row=1, padx=10)
-        self.open_button.grid(column=0, row=2, sticky=tk.NSEW, padx=10)
-        self.setup_button.grid(column=1, row=2, sticky=tk.NSEW, padx=10)
+        self.help_label.grid(column=0, row=0, columnspan=2, padx=10)
+        self.text_label.grid(column=0, row=1, columnspan=2, padx=10)
+        self.open_button.grid(column=0, row=2, sticky=tk.NSEW, padx=10, pady=10)
+        self.setup_button.grid(column=1, row=2, sticky=tk.NSEW, padx=10, pady=10)
 
 
 def main() -> None:
     root = tk.Tk()
     root.title("Grafs")
-    root.geometry("600x200")
+    # root.geometry("600x200")
+    
+    w = root.winfo_screenwidth()
+    h = root.winfo_screenheight()
+    w = w // 2  # середина экрана
+    h = h // 2
+    w = w - 200  # смещение от середины
+    h = h - 200
+    root.geometry(f'600x200+{w}+{h}')
+    root.resizable(width=False, height=False) 
+    
     root.grid_rowconfigure(index=0, weight=1)
     root.grid_columnconfigure(index=0, weight=1)
     root.grid_columnconfigure(index=1, weight=1)
